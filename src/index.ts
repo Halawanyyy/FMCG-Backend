63d5acf1dc004ce4c4dfd99a972b84a3f76b24ec
Halawanyyy <alhlwany951@gmail.com>
2024-05-08 23:22:22 +0300

    ha

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-// src/index.ts
-import express from 'express';
+// // src/index.ts
+// import express from 'express';
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello, TypeScript with Express!');
+// });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
